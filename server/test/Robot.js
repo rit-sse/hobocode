9034b4ca80c7a3d1fcba900449e7bf05ef2f9241
Brian Besmanoff <bbesmanoff@gmail.com>
2016-01-07 15:37:01 -0500

    Provide a test helper method for `validate` logic
    This abstraction may be extreme, but it allows for chaining of validity
    checks within tests.  It also serves to document the somewhat unexpected
    return values of `Instance#validate()`.

--- server/test/Robot.js
+++ server/test/Robot.js
@@ -1,11 +1,28 @@
 'use strict'
 
 let Robot = require('../models').Robot;
-require('chai-as-promised');
+
+/**
+ * Asserts that a response from `Instance#validate` is valid.  If it is, this
+ * will return a resolved Promise.  Otherwise, it will return a rejected
+ * Promise.  If you are testing that an instance is invalid, you must `catch`
+ * the resulting promise and negate it.
+ *
+ * Sequelize will return either a `null` or a `ValidationError` - `null` being
+ * the success case.
+ */
+let assertValidity = function(response) {
+  return response === null ? Promise.resolve() : Promise.reject();
+};
+
 describe('Robot', function() {
   describe('#save()', function () {
     it('should require a name', function () {
-      return Robot.build({}).validate().should.eventually.not.be.null;
+      return Robot.build({}).validate().then(assertValidity).catch(function() {
+        // it is expected for `assertValidity` to reject this call as an invalid
+        // Robot.
+        return Promise.resolve();
+      });
     });
   });
 });
