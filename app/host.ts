e19408ea9efb6b6aaea9ae74768c032cb4855ec2
Matt <mjs7769@rit.edu>
2016-01-12 15:45:26 -0500

    remove unneeded files

--- app/host.ts
+++
@@ -1,9 +0,0 @@
-var worker = new Worker("robot.js");
-function init(bot:string) {
-    console.log(bot);
-    worker.postMessage({"msg":"newBot", "bot":bot});
-}
-
-worker.addEventListener('message', function(e) {
-    document.getElementById('result').textContent = "Hello world!";
-}, false);
