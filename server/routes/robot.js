cbe94dcae569c0cac50a8516662e882471b6e24d
Wade Mauger <wam9729@rit.edu>
2016-01-13 17:45:10 -0500

    Add post and put methods to the Robot Route

--- server/routes/robot.js
+++ server/routes/robot.js
@@ -4,15 +4,44 @@ const express = require('express');
 const router = express.Router();
 const Robot = require('../models').Robot;
 
-router.route('/robots/:botname')
+router.route('/robots/:botname/:code?')
+
+  /* get a robot from the database */
   .get((req, res)=>{
     const botname = req.params.botname;
-    //res.send(botname);
     Robot.findAll({ where: { url_name: botname } }).then((robots)=>{
-      res.send(robots[0]);
+      console.log(robots);
+      res.json(robots[0]);
     });
   })
-  .put(()=>{
+
+  /* add a new robot to the database */
+  .post((req, res)=> {
+    const botname = req.params.botname;
+    const botcode = req.params.code;
+    Robot.create({ name: botname, code: botcode }).then(robot=> {
+      //success
+      res.send(robot.toJSON());
+    }).catch((err)=> {
+      //error
+      res.send(err);
+    });
+  })
+
+  /* update an existing robot in the database */
+  .put((req, res)=>{
+    const botname = req.params.botname;
+    const botcode = req.params.code;
+    //get the existing robot
+    Robot.findOne({ where: { url_name: botname } }).then((robot)=>{
+      robot.updateAttributes({ code: botcode }).then((robot)=>{
+        //success
+        res.send(robot.toJSON());
+      });
+    }).catch((err)=>{
+      //error
+      res.send(err);
+    });
   });
 
 module.exports =  router;
