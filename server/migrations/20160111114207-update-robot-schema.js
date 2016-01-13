715d4bf6d074871c667d2052466f0045e22b80bc
Wade Mauger <wam9729@rit.edu>
2016-01-13 13:57:38 -0500

    Revert migrations to promise.then() version

--- server/migrations/20160111114207-update-robot-schema.js
+++ server/migrations/20160111114207-update-robot-schema.js
@@ -1,27 +1,37 @@
 'use strict';
 
-var Promise = require('bluebird');
-
 module.exports = {
   up: function (queryInterface, Sequelize) {
     /*
-      This uses bluebird's `Promise.each` to run each modification sequentially
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+      
+      Race conditions may exist between migration steps. Promise.then()
+      mitigates these issues.
+       
     */
-    return Promise.each([
-      queryInterface.removeColumn('Robots', 'revision'),
-      queryInterface.changeColumn('Robots', 'url_name', {type:Sequelize.STRING, unique: true}),
-      queryInterface.addColumn('Robots', 'code', {type: Sequelize.STRING})
-    ]);
+      return queryInterface.removeColumn('Robots', 'revision').then(()=>{
+        queryInterface.changeColumn('Robots', 'url_name', {type:Sequelize.STRING, unique: true}).then(()=>{
+          queryInterface.addColumn('Robots', 'code', {type: Sequelize.STRING});
+        });
+      });
   },
 
   down: function (queryInterface, Sequelize) {
     /*
-      This uses bluebird's `Promise.each` to run each modification sequentially
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
     */
-    return Promise.each([
-      queryInterface.addColumn('Robots', 'revision', {type: Sequelize.INTEGER}),
-      queryInterface.changeColumn('Robots', 'url_name', {type:Sequelize.STRING, unique: false}),
-      queryInterface.removeColumn('Robots', 'code')
-    ]);
+      return queryInterface.addColumn('Robots', 'revision', {type: Sequelize.INTEGER}).then(()=>{
+        queryInterface.changeColumn('Robots', 'url_name', {type:Sequelize.STRING, unique: false}).then(()=>{
+          queryInterface.removeColumn('Robots', 'code');
+        });
+      });
   }
 };
